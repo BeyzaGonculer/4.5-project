db688f7ffae85a86d5a941c8b1519ee5b9f9272b
BeyzaGonculer <beyza.gonculer1@gmail.com>
2025-10-26 10:43:40 +0300

    order service

---
+++ order-service/src/main/java/com/turkcell/order_service/controller/OrdersController.java
@@ -0,0 +1,13 @@
+package com.turkcell.order_service.controller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrdersController {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrdersController.class, args);
+    }
+
+}
---
+++ order-service/src/main/java/com/turkcell/order_service/outbox/OutboxMessage.java
@@ -0,0 +1,5 @@
+package com.turkcell.order_service.outbox;
+
+public class OutboxMessage {
+
+}
