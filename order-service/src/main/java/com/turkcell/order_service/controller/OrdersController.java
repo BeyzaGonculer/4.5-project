3e33627535c9345871a0e6a4144eb6ad41ee1abd
BeyzaGonculer <beyza.gonculer1@gmail.com>
2025-10-26 10:48:47 +0300

    messaging

--- order-service/src/main/java/com/turkcell/order_service/controller/OrdersController.java
+++ order-service/src/main/java/com/turkcell/order_service/controller/OrdersController.java
@@ -1,13 +1,43 @@
 package com.turkcell.order_service.controller;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.cloud.stream.function.StreamBridge;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.support.MessageBuilder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
-@SpringBootApplication
+@RestController
+@RequestMapping("/api/v1/orders")
 public class OrdersController {
+    private final StreamBridge streamBridge;
 
-    public static void main(String[] args) {
-        SpringApplication.run(OrdersController.class, args);
+    public OrdersController(StreamBridge streamBridge) {
+        this.streamBridge = streamBridge;
     }
 
+    @PostMapping()
+    public String createOrder(@RequestBody CreateOrderDto dto) {
+        /// .........
+        OrderCreatedEvent event = new OrderCreatedEvent(dto.productId());
+
+        Message<OrderCreatedEvent> message = MessageBuilder.withPayload(event).build();
+
+        try {
+            boolean isSent = streamBridge.send("orderCreated-out-0", message);
+            if (!isSent)
+                System.out.println("Mesaj gönderilemedi");
+        } catch (Exception e) {
+            System.out.println("Mesaj gönderilemedi");
+        }
+
+        return dto.productId();
+    }
+
+    record CreateOrderDto(String productId) {
+    }
+
+    record OrderCreatedEvent(String productId) {
+    }
 }
--- order-service/src/main/java/com/turkcell/order_service/outbox/OutboxMessage.java
+++
@@ -1,5 +0,0 @@
-package com.turkcell.order_service.outbox;
-
-public class OutboxMessage {
-
-}
